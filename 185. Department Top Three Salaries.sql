db2985a329d49c2e567b1ddb980e8a9526da5f31
Liz Tianjiao Liu <tianjiaoliu2012@gmail.com>
2019-09-22 16:39:49 -0700

    Update 185. Department Top Three Salaries.sql

--- 185. Department Top Three Salaries.sql
+++ 185. Department Top Three Salaries.sql
@@ -1,14 +1,12 @@
-with cte as (
+with CTE as(
 select
-d.name as dep_name,
-e.name as emp_name, 
-e.salary,
-rank() over(partition by e.departmentid order by salary desc) as rk
-from employee e
-right join department d on e.departmentid = d.id
+d.name as department_name, e.name as employee, e.salary,
+dense_rank() over(partition by d.name order by salary desc) as rk
+from department d
+inner join employee e on e.departmentid = d.id
 )
 
-select dep_name as department, emp_name as employee, salary
-from cte
-where rk = 1
-order by dep_name, emp_name;
+select
+department_name as department, employee, salary
+from CTE
+where rk <=3;
