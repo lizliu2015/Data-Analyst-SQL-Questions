6580c646ae07e9e58089d3aad77a22b99b9cf92b
Liz Tianjiao Liu <tianjiaoliu2012@gmail.com>
2019-10-07 20:45:52 -0700

    Update 1193. Monthly Transactions I.sql

--- 1193. Monthly Transactions I.sql
+++ 1193. Monthly Transactions I.sql
@@ -21,3 +21,17 @@ coalesce(approved_total_amount,0) as approved_total_amount
 from CTE_all al
 left join CTE_approved ap 
 on al.month = ap.month and al.country = ap.country
+
+
+
+# case when
+select
+left(trans_date, 7) as month,
+country,
+count(*) as trans_count,
+sum(case when state = 'approved' then 1 else 0 end) as approved_count,
+sum(amount) as trans_total_amount,
+sum(case when state= 'approved' then amount else 0 end) as approved_total_amount
+from transactions
+group by left(trans_date, 7), country
+order by month
