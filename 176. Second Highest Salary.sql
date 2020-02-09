8bd0345c0653bc51dcf049545270e689a40985a4
Liz Tianjiao Liu <tianjiaoliu2012@gmail.com>
2020-02-09 10:49:53 -0800

    Update 176. Second Highest Salary.sql

--- 176. Second Highest Salary.sql
+++ 176. Second Highest Salary.sql
@@ -1,17 +1,48 @@
-#window funciton
-with CTE as (
-select
+Write a SQL query to get the second highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+For example, given the above Employee table, the query should return 200 as the second highest salary. If there is no second highest salary, then the query should return null.
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+
+Goal of this exercise: 
+1. get the second highest
+2. deal with null value
+
+Method 1:
+SELECT
+    IFNULL(
+      (SELECT DISTINCT Salary
+       FROM Employee
+       ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1),
+    NULL) AS SecondHighestSalary;
+
+Method 2:
+SELECT MAX(salary) as SecondHighestSalary
+FROM employee
+WHERE salary < (select MAX(salary) FROM employee)';
+
+
+#Method 3 - window funciton
+with CTE AS (
+SELECT
 salary,
-rank() over (order by salary) as rk
-from employee
+rank() over (ORDER BY salary) AS rk
+FROM employee
 )
 
-select
-salary as secondhighestsalary
-from CTE
-where rk = 2;
-
-#subquery
-select max(salary) as SecondHighestSalary
-from employee
-where salary < (select max(salary) from employee);
+SELECT
+salary AS secondhighestsalary
+FROM CTE
+WHERE rk = 2;
